e903404f7ea8f3ad789e1c32591d20d75f1381a2
Sayefdeen Alrawad <sayefdeen.alrawad@gmail.com>
2020-10-21 15:42:49 +0300

    model is generic

--- __tests__/model.test.js
+++ __tests__/model.test.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const middleModel = require('../middleware/model');
-const productCollection = require('../lib/models/products/products.collection');
-const cateCollection = require('../lib/models/categories/categories.collection');
+const productCollection = require('../lib/models/products/product.model');
+const cateCollection = require('../lib/models/categories/catigories.models');
 
 describe('Model Middleware Test', () => {
   it('if products was passed in the call model should be productsModel ', () => {
@@ -15,7 +15,7 @@ describe('Model Middleware Test', () => {
     const next = jest.fn();
 
     middleModel(req, res, next);
-    expect(req.model).toBeInstanceOf(productCollection);
+    expect(req.model).toBe(productCollection);
   });
 
   it('if categories was passed in the call model should be categoriesModel ', () => {
@@ -28,7 +28,7 @@ describe('Model Middleware Test', () => {
     const next = jest.fn();
 
     middleModel(req, res, next);
-    expect(req.model).toBeInstanceOf(cateCollection);
+    expect(req.model).toBe(cateCollection);
   });
 
   it('should response with 500 if anything else was passed ', () => {
